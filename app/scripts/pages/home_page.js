93515d31eec491ffcf51a6aeff7da266ed8ffab6
Kelly Milligan <kelly@milligan.net.nz>
2016-05-16 20:50:56 +0200

    Initial commit, merging separate templates from BitBucket into single repo with multiple example children, tidying

---
+++ app/scripts/pages/home_page.js
@@ -0,0 +1,63 @@
+define([
+
+    'lodash',
+    'jquery',
+    'TweenMax',
+    '_animation',
+    '_baseObject',
+
+    './modules/_example'
+
+], function(
+
+    _,
+    $,
+    TweenMax,
+    Animation,
+    BaseObject,
+
+    ExampleChild
+
+) { 'use strict';
+
+    return _.assign( _.create( BaseObject ), {
+
+
+        $example     : null,
+        exampleChild : null,
+
+
+        setup: function () {
+
+            this.$example = this.$node.find('.js-example');
+
+            var config = {
+                'index': 0,
+                'awesome': true
+            };
+
+            this.exampleChild = this.createChild( ExampleChild, this.$example, config );
+        },
+
+        resize: function () {
+
+            this.exampleChild.resize();
+        },
+
+        mouseMove: function () {
+
+            this.exampleChild.mouseMove();
+        },
+
+        onAnimFrame: function () {
+
+            this.exampleChild.animFrame();
+        }
+
+        // routeHome: function () {
+
+        // }
+
+    });
+
+});
