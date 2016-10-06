d8f47995d46600d7494d1967ad0117fc0add718b
Kelly Milligan <kelly@milligan.net.nz>
2016-10-06 18:42:25 +0200

    Starting on migration of threeJS example

--- src/scripts/modules/_example-three.js
+++ src/scripts/modules/_example-three.js
@@ -1,94 +1,76 @@
-define([
+import { _, $, BaseObject } from '../common';
 
-    'lodash',
-    'jquery',
-    'TweenMax',
-    '_baseObject',
+import * as THREE from 'three';
 
-    'three'
-
-], function(
-
-    _,
-    $,
-    TweenMax,
-    BaseObject,
-
-    THREE
-
-) { 'use strict';
-
-    return _.assign( _.create( BaseObject ), {
+export default Object.assign( Object.create( BaseObject ), {
 
 
-        renderer    : null,
-        scene       : null,
-        camera      : null,
+    renderer    : null,
+    scene       : null,
+    camera      : null,
 
-        origin      : null,
+    origin      : null,
 
-        material    : null,
-        mesh        : null,
+    material    : null,
+    mesh        : null,
 
 
-        setup: function () {
+    setup: function () {
 
-            this.renderer = new THREE.WebGLRenderer({ antialias: true });
-            this.renderer.setSize( this.windowData.width, this.windowData.height );
-            this.renderer.setClearColor( 'rgb(37, 26, 48)', 1 );
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize( this.windowData.width, this.windowData.height );
+        this.renderer.setClearColor( 'rgb(37, 26, 48)', 1 );
 
-            this.scene = new THREE.Scene();
+        this.scene = new THREE.Scene();
 
-            this.camera = new THREE.PerspectiveCamera( 75, width / height, 1, 1000 );
-            this.camera.position.z = 100;
+        this.camera = new THREE.PerspectiveCamera( 75, this.windowData.width / this.windowData.height, 1, 1000 );
+        this.camera.position.z = 100;
 
-            this.$node.append( this.renderer.domElement );
+        this.node.append( this.renderer.domElement );
 
-            this.setupActors();
+        this.setupActors();
 
-            this.resize();
-            this.render();
-        },
+        this.resize();
+        this.render();
+    },
 
-        setupActors: function () {
+    setupActors: function () {
 
-            this.material = new THREE.MeshBasicMaterial(
-            {
-                'color': 0x00ff00,
-                'wireframe': true
-            });
+        this.material = new THREE.MeshBasicMaterial(
+        {
+            'color': 0x00ff00,
+            'wireframe': true
+        });
 
-            this.mesh = new THREE.Mesh( new THREE.SphereGeometry( 5, 32, 32 ), this.material );
+        this.mesh = new THREE.Mesh( new THREE.SphereGeometry( 5, 32, 32 ), this.material );
 
-            this.origin = new THREE.Object3D();
+        this.origin = new THREE.Object3D();
 
-            this.origin.add( this.mesh );
-            this.scene.add( this.origin );
-        },
+        this.origin.add( this.mesh );
+        this.scene.add( this.origin );
+    },
 
-        resize: function () {
+    resize: function () {
 
-            this.camera.aspect = this.windowData.ratio;
-            this.camera.updateProjectionMatrix();
+        this.camera.aspect = this.windowData.ratio;
+        this.camera.updateProjectionMatrix();
 
-            this.renderer.setPixelRatio( window.devicePixelRatio );
-            this.renderer.setSize( this.windowData.width, this.windowData.height );
-        },
+        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.setSize( this.windowData.width, this.windowData.height );
+    },
 
-        mouseMove: function () {
+    mouseMove: function () {
 
-        },
+    },
 
-        onAnimFrame: function () {
+    onAnimFrame: function () {
 
-            this.render();
-        },
+        this.render();
+    },
 
-        render: function () {
+    render: function () {
 
-            this.renderer.render( this.scene, this.camera );
-        }
-
-    });
+        this.renderer.render( this.scene, this.camera );
+    }
 
 });
--- src/scripts/pages/home_page.js
+++ src/scripts/pages/home_page.js
@@ -1,25 +1,33 @@
 import { _, $, BaseObject } from '../common';
 
+import example from '../modules/_example-three';
+
 export default Object.assign( Object.create( BaseObject ), {
 
 
     //
 
+    test: null,
+
 
     setup: function () {
 
+        this.test = this.createChild( example, $('.js-example') );
     },
 
     resize: function () {
 
+        this.test.resize();
     },
 
     mouseMove: function () {
 
+        this.test.mouseMove();
     },
 
     onAnimFrame: function () {
 
+        this.test.animFrame( this.time );
     }
 
 });
